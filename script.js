b58ae8d5fd50e34093b8493c14a485792435b695
Nikita Ivanchikov <37267368+norito32@users.noreply.github.com>
2020-08-09 11:35:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let convertClickFunc = () => {
+    let text = document.getElementById('input_text');
+    text.classList.add('magic_effect');
+    setTimeout(() => text.classList.remove('magic_effect'), 2000);
+    let converteredEl = document.getElementsByClassName('converted_text')[0];
+    let converteredText = text.value;
+    if (document.getElementsByClassName('new_element')[0] === undefined)  // для проверки дочерних узлов - elem.hasChildNodes()
+    {
+        let div = document.createElement('div');
+        div.className = "new_element";
+        div.innerHTML = converteredText;
+        converteredEl.append(div);
+        div.classList.add('magic_effect_invert');
+        setTimeout(() => div.classList.remove('magic_effect_invert'), 2000)
+    }
+    else 
+    { 
+        let existDiv = document.getElementsByClassName('new_element')[0];
+        existDiv.innerHTML = converteredText;
+        div.classList.add('magic_effect_invert');
+        setTimeout(() => div.classList.remove('magic_effect_invert'), 2000)
+    }
+    
+}
+let clearLeft = () => {
+    let existingText = document.getElementById('input_text');
+    existingText.value = '';
+}
+let clearRight = () => {
+    let existingText = document.getElementsByClassName('new_element')[0];
+    existingText.remove();
+}
+
+let setColor = () => {
+        var color = document.getElementById('colorSelect');
+        let existingDiv = document.getElementsByClassName('new_element')[0];
+        if (color.value == "black"){
+            existingDiv.style.color = 'black'
+        } else if (color.value == "blue") {
+            existingDiv.style.color = '#2E97F2'
+        } else if (color.value == "coral") {
+            existingDiv.style.color = 'coral'
+        }
+
+}
+
+
+// </br>Новый <i>текст</i> он <b>важный</b> самый!
