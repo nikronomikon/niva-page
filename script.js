45b7e5e92212338a3120d4f67877ff17a13725eb
Nikita Ivanchikov <37267368+norito32@users.noreply.github.com>
2020-08-24 22:09:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,18 +17,6 @@ let func = () => {
     if (target.id =='percent') {
         display.innerHTML = percentFunc(display.innerHTML);
     }
-    if (target.id =='divide') {
-        display.innerHTML = divideFunc(display.innerHTML);
-    }
-    if (target.id =='multiply') {
-        display.innerHTML = multiplyFunc(display.innerHTML);
-    }
-    if (target.id =='minus') {
-        display.innerHTML = minusFunc(display.innerHTML);
-    }
-    if (target.id =='plus') {
-        display.innerHTML = plusFunc(display.innerHTML);
-    }
     if (target.id =='result') {
         altDisp.classList.remove('hidden');
         altDisp.innerHTML = resultFunc(display.innerHTML);
@@ -45,27 +33,10 @@ let percentFunc = (num) => {
     num = eval(num) / 100;
     return num;
 }
-let divideFunc = (num) => {
-    num = num + '/';
-    return num;
-}
-let multiplyFunc = (num) => {
-    num = num + '*';
-    return num;
-}
-let minusFunc = (num) => {
-    num = num + '-';
-    return num;
-}
-let plusFunc = (num) => {
-    num = num + '+';
-    return num;
-}
 let resultFunc = (num) => {
     num = Math.round(eval(num)*100)/100;
     return num;
 }
-
 
 calc.addEventListener('click', func);
 
